9f82291d2cb3730b601a5bfad17745f734bc108f
Alexy de Almeida <alexx.da95@gmail.com>
2022-11-04 21:45:33 +0000

    Initial commit

---
+++ src/scenes/Game.js
@@ -0,0 +1,19 @@
+import Phaser from '../lib/phaser.js'
+
+export default class Game extends Phaser.Scene {
+
+    constructor() {
+        super('game')
+    }
+
+    preload() {
+
+        this.load.image('background', 'assets/Background/bg_layer1.png')
+
+    }
+    create() {
+
+        this.add.image(240, 320, 'background')
+
+    }
+}
