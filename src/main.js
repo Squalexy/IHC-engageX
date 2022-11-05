55b229bf5bb7db7a391eac53ad9a18edd92e8d6b
Alexy de Almeida <alexx.da95@gmail.com>
2022-11-05 17:34:44 +0000

    tilemap with sprite moving

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import Game from './scenes/Game.js'
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    width: 480,
-    height: 640,
-    scene: Game
+    width: 800,
+    height: 600,
+    scene: [Game],
+    parent: 'main',
+    backgroundColor: '#1a1a2d',
+    pixelArt: true,
 })
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,12 +8,59 @@ export default class Game extends Phaser.Scene {
 
     preload() {
 
-        this.load.image('background', 'src/assets/Background/bg_layer1.png')
+        this.load.image('bunny', 'src/assets/sprites/bunny2_ready.png')
+        this.load.image('tiles', 'src/assets/tiles/desert.png')
+        this.load.tilemapTiledJSON('map', 'src/assets/tiles/map1.json')
 
     }
+
     create() {
 
-        this.add.image(240, 320, 'background')
+        this.cameras.main.setZoom(0.7)
+
+        // ----------------------------------------------------- TILEMAP CREATION //
+
+        const map = this.make.tilemap({
+            key: 'map'
+        }, 32, 32)
+        const tileset = map.addTilesetImage('desert', 'tiles', 32, 32)
+        const layer = map.createLayer('toplayer', tileset)
+
+        // ----------------------------------------------------- SPRITES CREATION //
+
+        const player = this.add.image(32+16, 32+16, 'bunny')
+        player.setScale(0.2)
+
+        // ----------------------------------------------------- BUTTONS //
+
+        //  Left
+        this.input.keyboard.on('keydown-A', function (event) {
+            const tile = layer.getTileAtWorldXY(player.x - 32, player.y, true);
+            if (tile.index === 2) player.x -= 0
+            else player.x -= 32
+        })
+
+        //  Right
+        this.input.keyboard.on('keydown-D', function (event) {
+            const tile = layer.getTileAtWorldXY(player.x + 32, player.y, true)
+            if (tile.index === 2) player.x += 0
+            else player.x += 32
+        })
+
+            //  Up
+        this.input.keyboard.on('keydown-W', function (event) {
+            const tile = layer.getTileAtWorldXY(player.x, player.y - 32, true)
+            if (tile.index === 2) player.y -= 0
+            else player.y -= 32
+        })
+
+        //  Down
+        this.input.keyboard.on('keydown-S', function (event) {
+            const tile = layer.getTileAtWorldXY(player.x, player.y + 32, true)
+            if (tile.index === 2) player.y += 0
+            else player.y += 32
+        })
+
 
     }
 }
