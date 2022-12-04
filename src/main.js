c4cf98bfdaffb95c8a850871ce05f3220620a465
Alexy de Almeida <alexx.da95@gmail.com>
2022-12-04 21:41:50 +0000

    Revert "Merge branch 'main' into Bot"
    This reverts commit f720bc63d333e7585fd08022deece2251515f446, reversing
    changes made to 71bc5050be5a68c7fd131df5bc0290c41904cf57.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Phaser from './lib/phaser.js'
 import Game from './scenes/Game.js'
-import HowToPlay from './scenes/HowToPlay.js'
 import IntroMenu from './scenes/IntroMenu.js'
-
 
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    scene: [IntroMenu, Game, HowToPlay],
+    scene: [IntroMenu, Game],
     parent: 'main',
     backgroundColor: '#241A0B',
     pixelArt: true,
--- src/scenes/HowToPlay.js
+++ src/scenes/HowToPlay.js
@@ -1,44 +1,36 @@
 
 export default class HowToPlay extends Phaser.Scene {
     constructor () {
-        super('HowToPlay')
+        super('howToPlay')
     }
 
 
     preload(){
-        this.load.image('backgroundHowToPlay', 'src/assets/Menus/howToPlay.png')
-
-        this.load.image('backButton1', 'src/assets/Menus/back.png');
-
     }
 
     create(){
-        this.background = this.add.sprite(0,0,'backgroundHowToPlay').setOrigin(0);
-
-        this.text = "                           There are 3 basic abilities:\n"           +
-                    "                               - Sow      (-33% LP)\n"                  +
-                    "                               - Harvest  (+ LP)\n"                     +
-                    "                               - Save     (LP -> XP)\n\n"               +
-                    "                The first 2 abilities allow you to plant and \n"                +
-                    "       collect lifepoints. The third one converts lifepoints into XP\n\n"         +
-                    "              If a player gets down to 0 lifepoints, loses\n\n"    +
-                    "When 2 players meet in adjacent cells, there is an encounter event.\n"+ 
-                    "     There you are abble to choose one of the following actions:\n\n"             +
-                    "             - Share: Both give half LP to one another\n"         +
-                    "             - Fight: Both spend 50% LP. One wins and\n"          +
-                    "                    takes 50% of the sum\n"                     +
-                    "             - Flee: Lose 25% LP and leave the encounter\n"       +
-                    "             - Steal: 25% Pb to steal 25% LP. Else lose\n\n"      +
-                    "             When the clock reaches 0, the game ends"          ;
+        this.text = "           There are 3 basic abilities:\n\n"           +
+                    "              - Sow      (-33% LP)\n"                  +
+                    "              - Harvest  (+ LP)\n"                     +
+                    "              - Save     (LP -> XP)\n\n"               +
+                    "              The first 2 abilities \n"                +
+                    "             allow you to plant and \n"                +
+                    "           collect lifepoints. The third\n"            +
+                    "          one converts lifepoints into XP\n\n"         +
+                    "  The player with most XP points wins the game\n"      +
+                    "  If a player gets down to 0 lifepoints, loses\n\n"    +
+                    "  When 2 players meet in a cell, there is an\n"        + 
+                    "  encounter event. The first player to choose\n"       + 
+                    "  forces one of the following actions:\n\n"            +
+                    "  - Share: Both give half LP to one another\n"         +
+                    "  - Fight: Both spend 50% LP. One wins and\n"          +
+                    "           takes 50% of the sum\n"                     +
+                    "  - Flee: Lose 25% LP and leave the encounter\n"       +
+                    "          Other may find it with 20% Pb\n"             +
+                    "  - Steal: 25% Pb to steal 25% LP. Else lose\n\n"      +
+                    "     When the clock reaches 0, the game ends"          ;
                     
-        this.body = this.add.text(200, 150, this.text, {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', fontSize: 16, color: "#241A0B"})
-        this.backButton1 = this.add.sprite(100,550,'backButton1')
-        .setInteractive().on('pointerdown', () => {
-            this.scene.transition({
-                target: 'IntroMenu',
-                duration: 0,
-            });
-        });
+        this.body = this.add.text(10, 30, this.text, {strokeThickness: 1, fontSize: 16, color: "#000000"})
        
     }
 }
